f8f22fa246f3d3ac9479193864dda090a2eda44e
Adam Cox <adam.cox@vorba.com>
2019-05-06 22:06:54 -0400

    add material and start first form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APIS } from '@vorba/tsi';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'tpc';
+
+  searchTypes: SearchType[] = [
+    { label: 'Account Number', value: 'account-number' },
+  ]
+  selectedSearchType = this.searchTypes[0];
+
+  matchTypes: MatchType[] = [
+    { label: 'Contains', value: 'contains' },
+  ]
+  selectedMatchType = this.matchTypes[0];
+
+  compareItems(i1, i2) {
+    return i1 && i2 && i1.id===i2.id;
+  }
+
+}
+
+export interface MatchType {
+  label: string;
+  value: string;
+}
+
+export interface SearchType {
+  label: string;
+  value: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from '../modules/material-module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  ],
+  bootstrap: [
+    AppComponent,
+  ]
 })
 export class AppModule { }
